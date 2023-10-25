e26529a6369e362ceeb42a260f4b205d7ef9165f
Krzyś <dyziob65@gmail.com>
2023-10-25 17:02:18 +0200

    Test

---
+++ src/main/java/com/gigatech/shop/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.gigatech.shop.repository;
+
+import com.gigatech.shop.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
