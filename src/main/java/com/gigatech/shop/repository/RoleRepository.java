222e54186ccb6a5d801803602ce05f7ad1628b8a
lukas <jasiniewicz@gmail.com>
2023-11-15 19:03:48 +0100

    html

--- src/main/java/com/gigatech/shop/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gigatech.shop.repository;
-
-import com.gigatech.shop.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/com/gigatech/shop/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gigatech.shop.repository;
-
-import com.gigatech.shop.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
